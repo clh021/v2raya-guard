d50bd7a7d53f8db2f0bf08337dac65fb06ea7642
chenlianghong <clh021@gmail.com>
2023-02-11 15:00:54 +0800

    main: fix build tag

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import v "github.com/clh021/v2raya-guard"
+import (
+	"fmt"
 
+	v "github.com/clh021/v2raya-guard"
+)
+
+// 支持启动时显示构建日期和构建版本
+// 需要通过命令 ` go build -ldflags "-X main.build=`git rev-parse HEAD`" ` 打包
+var build = "not set"
 func main() {
+	fmt.Printf("Build: %s\n", build)
 	v.Run()
 }
